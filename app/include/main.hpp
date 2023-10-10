35cabb3af9ec5a74919dc500d3d08115b6f3af51
tbardy <tbardy@witekio.com>
2023-10-09 20:53:51 -0700

    Hello world

---
+++ app/include/main.hpp
@@ -0,0 +1,13 @@
+/**
+ * @file      main.hpp
+ * @brief     main file
+ * @copyright Copyright (C) Theodore Bardy. All rights reserved.
+ *            Developed by Theodore Bardy.
+ *            Reproduction, copy, modification in whole or part is prohibited
+ *            without the written permission of the copyright owner.
+ */
+
+#ifndef _MAIN_HPP_
+#define _MAIN_HPP_
+
+#endif /* _MAIN_HPP_ */
---
+++ app/src/main.cpp
@@ -0,0 +1,19 @@
+/**
+ * @file      main.cpp
+ * @brief     main file
+ * @copyright Copyright (C) Theodore Bardy. All rights reserved.
+ *            Developed by Theodore Bardy.
+ *            Reproduction, copy, modification in whole or part is prohibited
+ *            without the written permission of the copyright owner.
+ */
+
+#include "main.hpp"
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+  cout << "Hello world !" << endl;
+
+  return 0;
+}
