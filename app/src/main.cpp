4c5d78ef9771ebd1755aeaf7557c14771e104468
Theodore-Bardy <bardy.theodore81@gmail.com>
2024-04-01 15:18:45 -0700

    Update demo application

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -6,12 +6,131 @@
 #include "main.hpp"
 #include <iostream>
 
+#include "board.hpp"
+
 using namespace std;
+
+/**
+ * @brief Convert move string into [X,Y] position
+ */
+static bool computeMove(string const& move, int& x, int& y);
 
 int
 main(int argc, char const* argv[])
 {
-    cout << "Hello world !" << endl;
+    Board gameBoard;
+    gameBoard.startUp();
+
+    cout << "   Chess core demo:" << endl;
+    cout << gameBoard << endl;
+
+    Piece* p = nullptr;
+    string move;
+    int    x;
+    int    y;
+
+    while (true)
+    {
+        cout << "Please select a piece: ";
+        cin >> move;
+        if (computeMove(move, x, y))
+        {
+            if (gameBoard.selectPiece(&p, x, y))
+            {
+                cout << "Please select a square to move the " << *p << ": ";
+                cin >> move;
+                if (computeMove(move, x, y))
+                {
+                    if (gameBoard.movePiece(p, x, y))
+                    {
+                        cout << gameBoard << endl;
+                    }
+                    else
+                    {
+                        cout << "[Error] Unable to move the piece" << endl;
+                    }
+                }
+                else
+                {
+                    cout << "[Error] Wrong input" << endl;
+                }
+            }
+            else
+            {
+                cout << "[Error] Unable to select the piece" << endl;
+            }
+        }
+        else
+        {
+            cout << "[Error] Wrong input" << endl;
+        }
+    }
 
     return 0;
+}
+
+static bool
+computeMove(string const& move, int& x, int& y)
+{
+    switch (move.at(0))
+    {
+        case 'a':
+            x = SQUARE_X_A;
+            break;
+        case 'b':
+            x = SQUARE_X_B;
+            break;
+        case 'c':
+            x = SQUARE_X_C;
+            break;
+        case 'd':
+            x = SQUARE_X_D;
+            break;
+        case 'e':
+            x = SQUARE_X_E;
+            break;
+        case 'f':
+            x = SQUARE_X_F;
+            break;
+        case 'g':
+            x = SQUARE_X_G;
+            break;
+        case 'h':
+            x = SQUARE_X_H;
+            break;
+        default:
+            return false;
+    }
+
+    switch (move.at(1))
+    {
+        case '1':
+            y = SQUARE_Y_1;
+            break;
+        case '2':
+            y = SQUARE_Y_2;
+            break;
+        case '3':
+            y = SQUARE_Y_3;
+            break;
+        case '4':
+            y = SQUARE_Y_4;
+            break;
+        case '5':
+            y = SQUARE_Y_5;
+            break;
+        case '6':
+            y = SQUARE_Y_6;
+            break;
+        case '7':
+            y = SQUARE_Y_7;
+            break;
+        case '8':
+            y = SQUARE_Y_8;
+            break;
+        default:
+            return false;
+    }
+
+    return true;
 }
